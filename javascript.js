52521c291f860b66b97c7c0c80d544240d442f0e
MatS43 <149340797+MatS43@users.noreply.github.com>
2024-12-16 22:19:14 +0100

    Add logic for playing single round

--- javascript.js
+++ javascript.js
@@ -15,4 +15,19 @@ function getHumanChoice(){
     let answer = prompt("Rock, Paper or Scissors").toLowerCase();
     return answer;
 }
-
+let humanScore = 0;
+let computerScore = 0;
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        console.log(`You tie! ${humanChoice[0].toUpperCase()}${humanChoice.substring(1)} is equal to ${computerChoice}!`);
+    } else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "rock"){
+        console.log(`You win! ${humanChoice[0].toUpperCase()}${humanChoice.substring(1)} beats ${computerChoice}!`);
+        ++humanScore; 
+    } else {
+        console.log(`You lose! ${computerChoice[0].toUpperCase()}${computerChoice.substring(1)} beats ${humanChoice}!`);
+        ++computerScore;
+    }
+}
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+playRound(humanChoice, computerChoice);
