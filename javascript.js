c907625b8e41ab662b1fe49fae4f2ad2be4a0b5d
MatS43 <149340797+MatS43@users.noreply.github.com>
2024-12-31 03:51:40 +0100

    Add basic interface to the game

--- javascript.js
+++ javascript.js
@@ -15,9 +15,23 @@ function getHumanChoice(){
     let answer = prompt("Rock, Paper or Scissors").toLowerCase();
     return answer;
 }
-function playGame(){
+function showScore(humanScore, computerScore){
+    score.textContent = `Wins ${humanScore} Loses ${computerScore}`;
+}
+function showAction(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        action.textContent=`You tie! ${humanChoice[0].toUpperCase()}${humanChoice.substring(1)} is equal to ${computerChoice}!`;
+    } else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "rock"){
+        action.textContent=`You win! ${humanChoice[0].toUpperCase()}${humanChoice.substring(1)} beats ${computerChoice}!`;
+    } else {
+        action.textContent=`You lose! ${computerChoice[0].toUpperCase()}${computerChoice.substring(1)} beats ${humanChoice}!`;
+    }
+}
 let humanScore = 0;
 let computerScore = 0;
+let btnr= document.querySelector("#rock");
+let btnp = document.querySelector("#paper");
+let btns = document.querySelector("#scissors")
 function playRound(humanChoice, computerChoice){
     if(humanChoice === computerChoice){
         console.log(`You tie! ${humanChoice[0].toUpperCase()}${humanChoice.substring(1)} is equal to ${computerChoice}!`);
@@ -28,19 +42,22 @@ function playRound(humanChoice, computerChoice){
         console.log(`You lose! ${computerChoice[0].toUpperCase()}${computerChoice.substring(1)} beats ${humanChoice}!`);
         ++computerScore;
     }
-}
-    let round = 0;
-    while(round < 5){
-    playRound(getHumanChoice(), getComputerChoice());
-    ++round;
-    console.log(`Your score is ${humanScore}`);
-    console.log(`Computer score is ${computerScore}`);
-    }
-    if (humanScore>computerScore){
-        console.log("You won the game!")
-    } else if (computerScore>humanScore){
-        console.log("You lost the game!")
-    } else {
-        console.log("You tied with the computer!")
+    showScore(humanScore, computerScore);
+    showAction(humanChoice, computerChoice);
+    if (humanScore>=5 && computerScore<5){
+        score.textContent="You win the game";
+        humanScore=0;
+        computerScore=0;
+    }else if(computerScore>=5 && humanScore<5){
+        score.textContent="You lose the game";
+        humanScore=0;
+        computerScore=0;
     }
 }
+btnr.addEventListener("click", () =>playRound("rock", getComputerChoice()));
+btnp.addEventListener("click", () =>playRound("paper", getComputerChoice()));
+btns.addEventListener("click", () =>playRound("scissors", getComputerChoice()));
+let score = document.querySelector("#score");
+score.textContent = `Wins 0 Loses 0`
+let action = document.querySelector("#action");
+action.textContent="``````"
